4c7381dd20b837a7471712a4111cc09296f5aac3
Hanyel-Brenner <125314727+Hanyel-Brenner@users.noreply.github.com>
2024-09-25 23:40:42 -0300

    Delete src/interfaces directory

--- src/interfaces/i-cookie.ts
+++
@@ -1,8 +0,0 @@
-import { Cookie } from "../models/cookie";
-
-export interface ICookie{
-    find(cookieId : string) : Promise<Cookie>;
-    add(cookie : Cookie): Promise<string>;
-    remove(email : string) : Promise<void>;
-    verify(cookieId : string) : Promise<boolean>;
-}
--- src/interfaces/i-user-repo.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../models/user"
-
-export interface IUserRepo{
-    find(email: string): Promise<User>
-    add(user: User): Promise<string>
-    remove(email: string): Promise<void>
-    list(): Promise<User[]>
-}

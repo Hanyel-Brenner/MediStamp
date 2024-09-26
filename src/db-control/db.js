65330912a518198bb6dc9f6d3bb116ce546b236a
Hanyel-Brenner <125314727+Hanyel-Brenner@users.noreply.github.com>
2024-09-25 23:35:22 -0300

    Delete src/db-control directory

--- src/db-control/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var client_1 = require("@prisma/client");
-if (!global.prisma) {
-    global.prisma = new client_1.PrismaClient();
-}
-exports.default = global.prisma;
--- src/db-control/db.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-if (!global.prisma) {
-    global.prisma = new PrismaClient();
-}
-
-export default global.prisma;
